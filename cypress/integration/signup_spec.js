dd5f2b482222a0d0dbe928a17ec227093c25348b
Ian Ross <ianrossd@gmail.com>
2019-12-09 22:01:25 +0800

    uploaded template

---
+++ cypress/integration/signup_spec.js
@@ -0,0 +1,11 @@
+describe.skip('CY Test Name', function() {
+    
+    before(function() {
+        
+    })
+    
+    it('Describe What You Are Wanting To Accomplish', function() {
+        cy.get('#qa').click().type('Something')
+        cy.get('#qa').click()        
+    })
+})
